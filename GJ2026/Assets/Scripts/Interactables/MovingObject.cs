5d8c04b9e13c0dc38eedcd55c153265f8b358fe0
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2026-01-26 23:37:42 +0700

    add: trap and door

---
+++ GJ2026/Assets/Scripts/Interactables/MovingObject.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework.Interfaces;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    [SerializeField] private Transform transform1;
+    [SerializeField] private Transform transform2;
+
+    [SerializeField] private float speed;
+
+
+    [SerializeField]  private Vector2 targetPos;
+
+    [SerializeField] private  Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb  = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        targetPos = transform1.position;
+    }
+
+    void FixedUpdate()
+    {
+        
+        if((targetPos- (Vector2) transform.position).sqrMagnitude < 0.1f){
+            if((targetPos- (Vector2) transform1.position).sqrMagnitude < 0.1f)
+            {
+                targetPos = transform2.position;
+            }
+            else
+            {
+                targetPos = transform1.position;
+            }
+        }
+        else
+        {
+            rb.MovePosition(Vector2.MoveTowards(rb.position, targetPos, speed * Time.fixedDeltaTime));
+        }
+    }
+
+
+
+
+}
